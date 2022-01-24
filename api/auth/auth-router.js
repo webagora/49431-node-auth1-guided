268f252b030603a3c4a0fd8f041927748fd5ef95
Ladrillo <gabriel@cabrejas.net>
2022-01-24 19:01:43 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -7,7 +7,7 @@ router.post('/register', async (req, res, next) => {
     // pull creds from req.body
     const { username, password } = req.body
     // hash the password w/ bcrypt
-    const hash = bcrypt.hashSync(password, 8) // 2 ^ 8
+    const hash = bcrypt.hashSync(password, 10) // 2 ^ 8
     // store new user in db
     const newUser = { username, password: hash }
     const inserted = await User.add(newUser)
