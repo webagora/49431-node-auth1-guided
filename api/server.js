9b250ce646681c7211fa662f6baa7cb3c784f17d
Ladrillo <gabriel@cabrejas.net>
2022-01-24 18:21:17 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -7,9 +7,10 @@ const authRouter = require('./auth/auth-router.js')
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../client')))
+server.use(express.json())
 
 server.use('/api/users', usersRouter)
-server.use(express.json())
+server.use('/api/auth', authRouter)
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'))
